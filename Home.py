4cfa788932228b5ac4bb3d2ab0ac933b0e908071
Mohsin Imran <45521516+scimohsin@users.noreply.github.com>
2023-04-03 20:48:16 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,7 @@ st.set_page_config(
 )
 
 st.title("CIFAR-10 Multi-Level Classification and Reverse Image Search")
+st.markdown("---")
 st.subheader("Welcome to our image classification and reverse search webapp! Our tool is powered by a convolutional neural network (CNN) trained on the CIFAR-10 dataset, which allows us to classify images and perform reverse image searches.  ")
 
 st.markdown("**With our webapp, you can upload an image and our model will first classify it into one of two categories, which are Living and Non-Living and then secondly into ten categories, including airplane, automobile, bird, cat, deer, dog, frog, horse, ship, and truck.  \nYou can also perform a reverse search by uploading an image and our model will find images similar to it in our database. Give it a try and discover what our model can do!**")
