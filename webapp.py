3f203c253db0e57bf3cb6ec52bd5bf05f53aa0c5
Mohsin Imran <45521516+scimohsin@users.noreply.github.com>
2023-04-03 19:44:41 +0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,31 @@
 import streamlit as st
 
 st.write("Hello World kese ho")
+uploaded_file = st.file_uploader("Choose an image")
+if uploaded_file is not None:
+    if save_uploaded_file(uploaded_file):
+        # display the file
+        display_image = Image.open(uploaded_file)
+        st.image(display_image)
+        # feature extract
+        features = feature_extraction(os.path.join("uploads",uploaded_file.name),model)
+        #st.text(features)
+        # recommendention
+        indices = recommend(features,feature_list)
+        # show
+        col1,col2,col3,col4,col5 = st.columns(5)
 
+        with col1:
+            st.image(filenames[indices[0][0]])
+        with col2:
+            st.image(filenames[indices[0][1]])
+        with col3:
+            st.image(filenames[indices[0][2]])
+        with col4:
+            st.image(filenames[indices[0][3]])
+        with col5:
+            st.image(filenames[indices[0][4]])
+    else:
+        st.header("Some error occured in file upload")
 
 
