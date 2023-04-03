9c3c9a9c13dda6d562faffd6af375ebd3ecc0729
Mohsin Imran <45521516+scimohsin@users.noreply.github.com>
2023-04-03 21:01:07 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Home"
+    page_title="Home",
+    layout="wide"
 )
 
 st.title("CIFAR-10 Multi-Level Classification and Reverse Image Search")
