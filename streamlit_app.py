4485a2be84361db7dc09d6783db56d34ba316fa9
Mohsin Imran <45521516+scimohsin@users.noreply.github.com>
2023-08-24 23:01:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
-import requests
 
 st.set_page_config(
     page_title="Home"
@@ -8,7 +6,6 @@ st.set_page_config(
 
 st.title("CIFAR-10 Multi-Level Classification and Reverse Image Search")
 st.markdown("---")
-st_lottie(lottie_home)
 st.subheader("Welcome to our image classification and reverse search webapp! Our tool is powered by a convolutional neural network (CNN) trained on the :blue[CIFAR-10] dataset, which allows us to classify images and perform reverse image searches.  ")
 
 st.caption("With our webapp, you can upload an image and our model will first classify it into one of two categories, which are Living and Non-Living and then secondly into ten categories, including airplane, automobile, bird, cat, deer, dog, frog, horse, ship, and truck. You can also perform a reverse search by uploading an image and our model will find images similar to it in our database. Give it a try and discover what our model can do!")
